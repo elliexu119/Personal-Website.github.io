f930484204b71208eeaa3e7d1aec150b25799d38
Banana-icecreme <ellixu119@gmail.com>
2020-11-01 20:54:07 -0500

    updated stock

--- october 30, 2020/js.js
+++ october 30, 2020/js.js
@@ -140,7 +140,9 @@ function popupSetup(){
             </div>';
          $("#popup").append(content);
 
-         $("#" + keys[i].id + "modalp").load('https://raw.githubusercontent.com/elliexu119/Personal-Website/master/october%2030%2C%202020/projects/'+ keys[i].id + '.html');
+         $("#" + keys[i].id + "modalp").load('https://raw.githubusercontent.com/elliexu119/Personal-Website/master/october%2030%2C%202020/projects/'+ keys[i].id + '.html', function() {
+            $(this).trigger("pagecreate");
+        });
          //$("#" + keys[i].id + "modalp").load('https://raw.githubusercontent.com/elliexu119/Personal-Website/master/october%2030%2C%202020/'+ keys[i].id + '.html');
          
     }
